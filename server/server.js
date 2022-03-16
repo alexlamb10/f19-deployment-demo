dd6839224159f68a58c026a633e25270fc2f2aa0
Alex Lamb <alexlamb104@gmail.com>
2022-03-16 12:43:00 -0600

    Attempting to use images in another folder

--- server/server.js
+++ server/server.js
@@ -6,13 +6,26 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/index.html"))
-});
 
-app.get("/styles", function(req,res) {
-    res.sendFile(path.join(__dirname, "../public/index.css"))
-});
+//Middleware 
+//Use either middleware or endpoints NOT BOTH
+app.use(express.static(path.join(__dirname, "../public")))
+
+app.use(express.static(path.join(__dirname, "../images")))
+
+//Endpoints
+//Use this endpoint to host pictures
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public"))
+// })
+
+// app.get("/", function(req, res){
+//     res.sendFile(path.join(__dirname, "../public/index.html"))
+// });
+
+// app.get("/styles", function(req,res) {
+//     res.sendFile(path.join(__dirname, "../public/index.css"))
+// });
 
 const port = process.env.PORT || 4005;
 
